8418d6ed65262a34c6054f7bc8bf593634828858
Alex Gonopolskiy <agonopol@gmail.com>
2018-12-05 12:09:31 +0100

    [ag] fix test and formating

--- {{cookiecutter.repo_name}}/tests/_jobs/{{cookiecutter.job}}/test_{{cookiecutter.job}}.py
+++ {{cookiecutter.repo_name}}/tests/_jobs/{{cookiecutter.job}}/test_{{cookiecutter.job}}.py
@@ -3,8 +3,7 @@ from pysparkling import Context
 from jobs.{{cookiecutter.job}} import analyze
 
 @patch('jobs.{{cookiecutter.job}}.{{ cookiecutter.project.replace(' ', '').replace('-', '') }}Context.initalize_counter')
-@patch('jobs.{{cookiecutter.job}}.{{ cookiecutter.project.replace(' ', '').replace('-', '') }}JobContext.inc_counter')
+@patch('jobs.{{cookiecutter.job}}.{{ cookiecutter.project.replace(' ', '').replace('-', '') }}Context.inc_counter')
 def test_{{cookiecutter.job}}_analyze(_, __):
     result = analyze(Context())
     assert len(result) == 327
-    assert result[:6] == [('ut', 17), ('eu', 16), ('vel', 14), ('nec', 14), ('quis', 12), ('vitae', 12)]
